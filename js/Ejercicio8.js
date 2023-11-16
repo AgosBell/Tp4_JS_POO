2de6389dfcb3ab06b78fca7ba676eac04b653f7c
Agostina Bellotti <agostinabell@gmail.com>
2023-11-16 15:18:34 -0300

    Agregué el ejercicio n°8

---
+++ js/Ejercicio8.js
@@ -0,0 +1,16 @@
+/*Crea una clase llamada "Persona" que tenga las propiedades "nombre", "edad" y "profesión", y los métodos "saludar" y "despedirse".
+Luego, crea dos objetos de la clase "Persona" con diferentes valores para sus propiedades y llama a sus métodos "saludar" y "despedirse".*/
+
+const Persona = {
+    nombre : `María`,
+    edad: 30,
+    profesion: `estudiante`,
+}
+
+saludar: Function()
+    document.write(`<p>Hola soy ${Persona.nombre}</p>`)
+    
+despedirse: Function()
+    document.write(`<p>Hasta luego! Nos vemos!</p>`)
+
+    
